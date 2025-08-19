a311f464cb028afabbb32c687b51e5cee289e656
ShaneSerrao <multicorpsa@gmail.com>
2025-08-19 12:45:31 +0200

    Delete src directory

--- src/PulsNet.Web/Models/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace PulsNet.Web.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-        public bool TwoFactorEnabledGlobally { get; set; }
-    }
-}
--- src/PulsNet.Web/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace PulsNet.Web.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
